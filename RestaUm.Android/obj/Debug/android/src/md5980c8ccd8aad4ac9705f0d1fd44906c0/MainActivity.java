cfc2074f940a863c24916df7349a210db3b30d27
Ismael Silva <ismaelsilva.dev@outlook.com>
2019-01-05 18:28:26 -0300

    Primeiro comite

---
+++ RestaUm.Android/obj/Debug/android/src/md5980c8ccd8aad4ac9705f0d1fd44906c0/MainActivity.java
@@ -0,0 +1,47 @@
+package md5980c8ccd8aad4ac9705f0d1fd44906c0;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("RestaUm.Droid.MainActivity, RestaUm.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("RestaUm.Droid.MainActivity, RestaUm.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
